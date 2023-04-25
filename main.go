1e8a49b77e57717c733951545aed286462490dda
Rachit <rctrj@ip-192-168-0-178.ap-south-1.compute.internal>
2023-04-25 13:27:33 +0530

    add custom method for running nakama

--- main.go
+++ main.go
@@ -71,6 +71,160 @@ func Migrate(dbUrl string) {
 	}, tmpLogger)
 }
 
+func RunNakama(config server.Config, shutDown func(context.Context)) {
+	semver := fmt.Sprintf("%s+%s", version, commitID)
+	http.DefaultClient.Timeout = 1500 * time.Millisecond
+
+	tmpLogger := server.NewJSONLogger(os.Stdout, zapcore.InfoLevel, server.JSONFormat)
+	logger, startupLogger := server.SetupLogging(tmpLogger, config)
+	configWarnings := server.CheckConfig(logger, config)
+
+	startupLogger.Info("Nakama starting")
+	startupLogger.Info("Node", zap.String("name", config.GetName()), zap.String("version", semver), zap.String("runtime", runtime.Version()), zap.Int("cpu", runtime.NumCPU()), zap.Int("proc", runtime.GOMAXPROCS(0)))
+
+	// Initialize the global random with strongly seed.
+	var seed int64
+	if err := binary.Read(cryptoRand.Reader, binary.BigEndian, &seed); err != nil {
+		startupLogger.Warn("Failed to get strongly random seed, fallback to a less random one.", zap.Error(err))
+		seed = time.Now().UnixNano()
+	}
+	rand.Seed(seed)
+
+	redactedAddresses := make([]string, 0, 1)
+	for _, address := range config.GetDatabase().Addresses {
+		rawURL := fmt.Sprintf("postgres://%s", address)
+		parsedURL, err := url.Parse(rawURL)
+		if err != nil {
+			logger.Fatal("Bad connection URL", zap.Error(err))
+		}
+		redactedAddresses = append(redactedAddresses, strings.TrimPrefix(parsedURL.Redacted(), "postgres://"))
+	}
+	startupLogger.Info("Database connections", zap.Strings("dsns", redactedAddresses))
+
+	// Global server context.
+	ctx, ctxCancelFn := context.WithCancel(context.Background())
+
+	db, dbVersion := server.DbConnect(ctx, startupLogger, config)
+	startupLogger.Info("Database information", zap.String("version", dbVersion))
+
+	// Check migration status and fail fast if the schema has diverged.
+	migrate.StartupCheck(startupLogger, db)
+
+	// Access to social provider integrations.
+	socialClient := social.NewClient(logger, 5*time.Second, config.GetGoogleAuth().OAuthConfig)
+
+	// Start up server components.
+	cookie := newOrLoadCookie(config)
+	metrics := server.NewLocalMetrics(logger, startupLogger, db, config)
+	sessionRegistry := server.NewLocalSessionRegistry(metrics)
+	sessionCache := server.NewLocalSessionCache(config.GetSession().TokenExpirySec)
+	consoleSessionCache := server.NewLocalSessionCache(config.GetConsole().TokenExpirySec)
+	loginAttemptCache := server.NewLocalLoginAttemptCache()
+	statusRegistry := server.NewStatusRegistry(logger, config, sessionRegistry, jsonpbMarshaler)
+	tracker := server.StartLocalTracker(logger, config, sessionRegistry, statusRegistry, metrics, jsonpbMarshaler)
+	router := server.NewLocalMessageRouter(sessionRegistry, tracker, jsonpbMarshaler)
+	leaderboardCache := server.NewLocalLeaderboardCache(logger, startupLogger, db)
+	leaderboardRankCache := server.NewLocalLeaderboardRankCache(ctx, startupLogger, db, config.GetLeaderboard(), leaderboardCache)
+	leaderboardScheduler := server.NewLocalLeaderboardScheduler(logger, db, config, leaderboardCache, leaderboardRankCache)
+	googleRefundScheduler := server.NewGoogleRefundScheduler(logger, db, config)
+	matchRegistry := server.NewLocalMatchRegistry(logger, startupLogger, config, sessionRegistry, tracker, router, metrics, config.GetName())
+	tracker.SetMatchJoinListener(matchRegistry.Join)
+	tracker.SetMatchLeaveListener(matchRegistry.Leave)
+	streamManager := server.NewLocalStreamManager(config, sessionRegistry, tracker)
+	runtime, runtimeInfo, err := server.NewRuntime(ctx, logger, startupLogger, db, jsonpbMarshaler, jsonpbUnmarshaler, config, version, socialClient, leaderboardCache, leaderboardRankCache, leaderboardScheduler, sessionRegistry, sessionCache, statusRegistry, matchRegistry, tracker, metrics, streamManager, router)
+	if err != nil {
+		startupLogger.Fatal("Failed initializing runtime modules", zap.Error(err))
+	}
+	matchmaker := server.NewLocalMatchmaker(logger, startupLogger, config, router, metrics, runtime)
+	partyRegistry := server.NewLocalPartyRegistry(logger, matchmaker, tracker, streamManager, router, config.GetName())
+	tracker.SetPartyJoinListener(partyRegistry.Join)
+	tracker.SetPartyLeaveListener(partyRegistry.Leave)
+
+	leaderboardScheduler.Start(runtime)
+	googleRefundScheduler.Start(runtime)
+
+	pipeline := server.NewPipeline(logger, config, db, jsonpbMarshaler, jsonpbUnmarshaler, sessionRegistry, statusRegistry, matchRegistry, partyRegistry, matchmaker, tracker, router, runtime)
+	statusHandler := server.NewLocalStatusHandler(logger, sessionRegistry, matchRegistry, tracker, metrics, config.GetName())
+
+	apiServer := server.StartApiServer(logger, startupLogger, db, jsonpbMarshaler, jsonpbUnmarshaler, config, version, socialClient, leaderboardCache, leaderboardRankCache, sessionRegistry, sessionCache, statusRegistry, matchRegistry, matchmaker, tracker, router, streamManager, metrics, pipeline, runtime)
+	consoleServer := server.StartConsoleServer(logger, startupLogger, db, config, tracker, router, streamManager, metrics, sessionRegistry, sessionCache, consoleSessionCache, loginAttemptCache, statusRegistry, statusHandler, runtimeInfo, matchRegistry, configWarnings, semver, leaderboardCache, leaderboardRankCache, leaderboardScheduler, apiServer, runtime, cookie)
+
+	gaenabled := len(os.Getenv("NAKAMA_TELEMETRY")) < 1
+	console.UIFS.Nt = !gaenabled
+	const gacode = "UA-89792135-1"
+	var telemetryClient *http.Client
+	if gaenabled {
+		telemetryClient = &http.Client{
+			Timeout: 1500 * time.Millisecond,
+		}
+		runTelemetry(telemetryClient, gacode, cookie)
+	}
+
+	// Respect OS stop signals.
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	startupLogger.Info("Startup done")
+
+	// Wait for a termination signal.
+	<-c
+	shutDown(ctx)
+
+	graceSeconds := config.GetShutdownGraceSec()
+
+	// If a shutdown grace period is allowed, prepare a timer.
+	var timer *time.Timer
+	timerCh := make(<-chan time.Time, 1)
+	if graceSeconds != 0 {
+		timer = time.NewTimer(time.Duration(graceSeconds) * time.Second)
+		timerCh = timer.C
+		startupLogger.Info("Shutdown started - use CTRL^C to force stop server", zap.Int("grace_period_sec", graceSeconds))
+	} else {
+		// No grace period.
+		startupLogger.Info("Shutdown started")
+	}
+
+	// Stop any running authoritative matches and do not accept any new ones.
+	select {
+	case <-matchRegistry.Stop(graceSeconds):
+		// Graceful shutdown has completed.
+		startupLogger.Info("All authoritative matches stopped")
+	case <-timerCh:
+		// Timer has expired, terminate matches immediately.
+		startupLogger.Info("Shutdown grace period expired")
+		<-matchRegistry.Stop(0)
+	case <-c:
+		// A second interrupt has been received.
+		startupLogger.Info("Skipping graceful shutdown")
+		<-matchRegistry.Stop(0)
+	}
+	if timer != nil {
+		timer.Stop()
+	}
+
+	// Signal cancellation to the global runtime context.
+	ctxCancelFn()
+
+	// Gracefully stop remaining server components.
+	apiServer.Stop()
+	consoleServer.Stop()
+	matchmaker.Stop()
+	leaderboardScheduler.Stop()
+	googleRefundScheduler.Stop()
+	tracker.Stop()
+	statusRegistry.Stop()
+	sessionCache.Stop()
+	sessionRegistry.Stop()
+	metrics.Stop(logger)
+	loginAttemptCache.Stop()
+
+	if gaenabled {
+		_ = ga.SendSessionStop(telemetryClient, gacode, cookie)
+	}
+
+	startupLogger.Info("Shutdown complete")
+}
+
 func Run(dbUrl string, shutDown func(context.Context)) {
 	semver := fmt.Sprintf("%s+%s", version, commitID)
 	// Always set default timeout on HTTP client.
